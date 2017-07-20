7591707c027dce0ad6e112caf0e8774535dca59a
Sunjie.Wu <wusunjie@pset.suntec.net>
2017-07-20 08:56:39 +0800

    use memcpy to assign to struct.

--- Adapter/GpsAdapter.cpp
+++ Adapter/GpsAdapter.cpp
@@ -135,20 +135,10 @@ static int GpsAdapterInit(GpsCallbacks *cb)
         | GPS_LOCATION_HAS_SPEED | GPS_LOCATION_HAS_BEARING |
         GPS_LOCATION_HAS_ACCURACY,
     };
+
     memset(&context, 0, sizeof(AdapterThreadCtx));
 
-    callbacks.location_cb = cb->location_cb;
-    callbacks.status_cb = cb->status_cb;
-    callbacks.sv_status_cb = cb->sv_status_cb;
-    callbacks.nmea_cb = cb->nmea_cb;
-    callbacks.set_capabilities_cb = cb->set_capabilities_cb;
-    callbacks.acquire_wakelock_cb = cb->acquire_wakelock_cb;
-    callbacks.release_wakelock_cb = cb->release_wakelock_cb;
-    callbacks.create_thread_cb = cb->create_thread_cb;
-    callbacks.request_utc_time_cb = cb->request_utc_time_cb;
-    callbacks.set_system_info_cb = cb->set_system_info_cb;
-    callbacks.gnss_sv_status_cb = cb->gnss_sv_status_cb;
-    callbacks.size = sizeof(GpsCallbacks);
+    memcpy(&callbacks, cb, sizeof(GpsCallbacks));
 
     msgQueue = MsgQueueCreate();
     mainLoopEvent = GPSEventCreate();
